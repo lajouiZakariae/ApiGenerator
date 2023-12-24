45013f686bb0237749aaca7051384729e05ccd06
Lajoui Zakariae <lajoui.zakariae.1@gmail.com>
2023-12-24 16:11:14 +0100

    working on factories

--- src/ApiGeneratorServiceProvider.php
+++ src/ApiGeneratorServiceProvider.php
@@ -4,7 +4,6 @@ namespace Zakalajo\ApiGenerator;
 
 use Illuminate\Support\ServiceProvider;
 use Zakalajo\ApiGenerator\Commands\ApiCreate;
-use Zakalajo\ApiGenerator\Commands\FactoryCreate;
 use Zakalajo\ApiGenerator\Commands\FormRequestCreate;
 use Zakalajo\ApiGenerator\Commands\ModelCreate;
 use Zakalajo\ApiGenerator\Commands\ResourceCreate;
